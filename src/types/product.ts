6dbf257c4f9149fa27d4f0cf9fab7d6d4cf49290
ryosukeharada <web.harada1111@gmail.com>
2025-11-30 11:45:50 +0900

    fix productsインターフェースファイルのファイル名の修正

--- src/types/product.ts
+++ src/types/product.ts
@@ -1,11 +1,9 @@
-// 商品の型定義
+// 商品詳細ページの方定義
 export interface Product {
-  // 商品
   id: number;
   name: string;
   imageUrl: string;
   categories: Array<string>;
-  isPickUp: boolean;
   isNew: boolean;
   isEvent: boolean;
   price: number;
